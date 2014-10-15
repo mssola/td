b783a99060f801222645ab15b7720fb80b6cc4f9
Miquel Sabaté <mikisabate@gmail.com>
2014-10-15 16:31:42 +0200

    lib: added tests for the utils.go file.

--- lib/utils.go
+++ lib/utils.go
@@ -14,9 +14,14 @@ import (
 )
 
 const (
+	// The fallback editor in case the $EDITOR environment variable is not set.
 	defaultEditor = "vi"
 )
 
+// Returns the value of the current home. This value is fetched from the $TD
+// environment variable. If it's not set, then the $HOME environment variable
+// will be picked. If the $HOME environment variable is not set either,
+// then it panics.
 func home() string {
 	value := os.Getenv("TD")
 	if value == "" {
@@ -28,6 +33,8 @@ func home() string {
 	return value
 }
 
+// Returns the value of the $EDITOR environment variable. If this variable is
+// not set, then it will return the value of the "defaultEditor" constant.
 func editor() string {
 	value := os.Getenv("EDITOR")
 	if value == "" {
@@ -36,7 +43,9 @@ func editor() string {
 	return value
 }
 
-// TODO
+// Copy a file from a source path to a destination path. This function assumes
+// that the source path exists. The only error that can be tolerated is if the
+// user is trying to cpy a file into a protected directory.
 func copyFile(source string, dest string) error {
 	sf, _ := os.Open(source)
 	defer sf.Close()
@@ -49,12 +58,25 @@ func copyFile(source string, dest string) error {
 	return err
 }
 
-// TODO
+// Copy a given source directory into a destination directory. If the
+// destination directory already exists, it will be removed. This function will
+// only tolerate the following errors:
+//
+//	1. The copying of the files inside a directory has failed.
+//  2. The source directory cannot be read.
+//
+// Note that subdirectories will *not* be copied. This is because this is a
+// simple function that adjust to our scheme: directories only have regular
+// files inside.
 func copyDir(source string, dest string) error {
 	os.RemoveAll(dest)
 	os.MkdirAll(dest, 0755)
 
 	entries, err := ioutil.ReadDir(source)
+	if err != nil {
+		return fromError(err)
+	}
+
 	for _, entry := range entries {
 		sfp := filepath.Join(source, entry.Name())
 		dfp := filepath.Join(dest, entry.Name())
@@ -66,7 +88,9 @@ func copyDir(source string, dest string) error {
 	return nil
 }
 
-// TODO
+// Construct the URL for the given path. The second parameter "token" tells
+// this function whether it should include the authorization token in the
+// query.
 func requestUrl(path string, token bool) string {
 	u, _ := url.Parse(config.Server)
 	u.Path = path
@@ -78,6 +102,11 @@ func requestUrl(path string, token bool) string {
 	return u.String()
 }
 
+// Perform an HTTP request and get back the response. The "method" parameter
+// corresponds to an HTTP method (e.g. "GET") and the "url" parameter corresponds
+// to just the path for the URL (e.g. "/topics"). Some HTTP requests might want
+// to send data through the body of the request. In this case the "body"
+// parameter should be used.
 func getResponse(method, url string, body io.Reader) (*http.Response, error) {
 	client := &http.Client{}
 	str := requestUrl(url, true)
---
+++ lib/utils_test.go
@@ -0,0 +1,122 @@
+// Copyright (C) 2014 Miquel Sabaté Solà <mikisabate@gmail.com>
+// This file is licensed under the MIT license.
+// See the LICENSE file.
+
+package lib
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHome(t *testing.T) {
+	casa := os.Getenv("HOME")
+	assert.Equal(t, casa, home())
+
+	os.Setenv("TD", "/home/lala")
+	assert.Equal(t, "/home/lala", home())
+
+	defer func() {
+		if err := recover(); err != nil {
+			msg := "You don't have the $HOME environment variable set"
+			assert.Equal(t, msg, err)
+		}
+	}()
+	os.Setenv("HOME", "")
+	os.Setenv("TD", "")
+	home()
+
+	os.Setenv("HOME", casa)
+}
+
+func TestEditor(t *testing.T) {
+	os.Setenv("EDITOR", "emacs")
+	assert.Equal(t, editor(), "emacs")
+	os.Setenv("EDITOR", "")
+	assert.Equal(t, editor(), defaultEditor)
+}
+
+func TestCopyFile(t *testing.T) {
+	// Prepare the filesystem.
+	os.RemoveAll("/tmp/td")
+	os.MkdirAll("/tmp/td/good", 0755)
+	os.MkdirAll("/tmp/td/mordor", 0000)
+	f, err := os.Create("/tmp/td/good/test.txt")
+	defer f.Close()
+	assert.Nil(t, err)
+	f.WriteString("hello world")
+
+	// Good copy of the "good" dir.
+	err = copyDir("/tmp/td/good", "/tmp/td/lala")
+	assert.Nil(t, err)
+	body, err := ioutil.ReadFile("/tmp/td/lala/test.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, string(body), "hello world")
+
+	// Copying the same directory replaces the old one.
+	f.WriteString(" lala")
+	err = copyDir("/tmp/td/good", "/tmp/td/lala")
+	assert.Nil(t, err)
+	body, err = ioutil.ReadFile("/tmp/td/lala/test.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, string(body), "hello world lala")
+
+	// Trying from a directory that doesn't exist.
+	err = copyDir("/tmp/td/alderaan", "/tmp/td/lala")
+	assert.NotNil(t, err)
+
+	// One does not simply walk into Mordor...
+	err = copyDir("/tmp/td/good", "/tmp/td/mordor/good")
+	msg := "open /tmp/td/mordor/good/test.txt: permission denied"
+	assert.Equal(t, err.Error(), msg)
+
+	// ... but you can replace Mordor :D
+	err = copyDir("/tmp/td/good", "/tmp/td/mordor")
+	assert.Nil(t, err)
+	body, err = ioutil.ReadFile("/tmp/td/mordor/test.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, string(body), "hello world lala")
+}
+
+func TestRequestUrl(t *testing.T) {
+	config = &configuration{
+		Server: "http://localhost:9999/",
+		Token:  "1234",
+	}
+
+	path1 := requestUrl("lala", true)
+	path2 := requestUrl("/lala", true)
+	assert.Equal(t, path1, path2)
+	assert.Equal(t, path1, "http://localhost:9999/lala?token=1234")
+
+	path3 := requestUrl("lala", false)
+	assert.Equal(t, path3, "http://localhost:9999/lala")
+}
+
+func TestGetResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	config = &configuration{
+		Server: ts.URL,
+		Token:  "1234",
+	}
+
+	res, err := getResponse("GET", "/lala", nil)
+	assert.Nil(t, err)
+	assert.Equal(t, res.StatusCode, 200)
+
+	req := res.Request
+	assert.Equal(t, req.Method, "GET")
+	assert.Equal(t, req.URL.Path, "/lala")
+	assert.Equal(t, req.URL.RawQuery, "token=1234")
+	assert.Equal(t, req.Header.Get("Content-Type"), "application/json")
+}
